f54938fa19d99e7d9ade88588903bd4c399f00ab
Brandon Wamboldt <brandon.wamboldt@smartsti.com>
2013-02-28 15:39:29 -0400

    SYST command now retrieves the actual OS

--- commands/syst.js
+++ commands/syst.js
@@ -1,7 +1,9 @@
 exports = module.exports = (function(app) {
+    var os = require('os');
+
     app.server.on('ftp:command_received', function(command, parameters, output, session) {
         if (command === 'SYST') {
-            output.write(215, 'UNIX Type: ' + session.type);
+            output.write(215, os.type() + ' Type: ' + session.type);
         } 
     });
 });
