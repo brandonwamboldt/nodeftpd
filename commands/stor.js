4c6b24ae217eaacc5a4aeaa010973b6b2ef16acc
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-03-01 15:55:25 -0400

    Fixed the STOR function, now works with binary transfers

--- commands/stor.js
+++ commands/stor.js
@@ -4,11 +4,26 @@ var command = require('../lib/command')
 
 command.add('STOR', function (path, output, session) {
     var success = channel.create(session, function(socket, done) {
-        socket.on('data', function(data) {
-            fs.writeFile(session.cwd + '/' + path, data, function(err) {
-                output.write(226, "Transfer complete");
+        // @todo Write to file as it is received to avoid memory issues
+        var buffers = []
+          , bytes   = 0
+          , offset  = 0;
+
+        socket.on('data', function(buffer) {
+            buffers.push(buffer);
+            bytes += buffer.length;
+        });
+
+        socket.on('end', function(data) {
+            fs.open(session.cwd + '/' + path, 'w', function(err, fd) {
+                for (i in buffers) {
+                    fs.writeSync(fd, buffers[i], 0, buffers[i].length, offset);
+                    offset += buffers[i].length;
+                }
 
                 done();
+
+                output.write(226, 'Transfer Complete'); 
             });
         });
     });
@@ -16,6 +31,6 @@ command.add('STOR', function (path, output, session) {
     if (!success) {
         output.write(425, 'Unable to build data connection: Invalid argument'); 
     } else {
-        output.write(150, "Opening " + session.transferType + " mode data connection for " + path);
+        output.write(150, "Opening " + session.transferType.toUpperCase() + " mode data connection for " + path);
     }
 });
