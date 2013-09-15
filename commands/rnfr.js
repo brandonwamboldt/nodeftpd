bc8bfd3672d8b5cad0ccfac9b820af8bb6eef09a
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-15 12:44:01 -0300

    Adding rnfr/rnto commands to rename files

---
+++ commands/rnfr.js
@@ -0,0 +1,34 @@
+var command = require('../lib/command');
+var fs      = require('fs');
+
+command.add('RNFR', rnfr, {maxArguments: 1, minArguments: 1});
+
+/**
+ * Used when renaming a file. Use this command to specify the file to be
+ * renamed; follow it with an RNTO command to specify the new name for the file.
+ * @param {!string} fromFilename
+ * @param {!object} output
+ * @param {!object} session
+ */
+function rnfr(fromFilename, output, session) {
+  // If from name is a relative path, prepend the CWD to it to get an absolute
+  // path
+  if (fromFilename[0] !== '/') {
+    if (session.cwd === '/') {
+      fromFilename = session.cwd + fromFilename;
+    } else {
+      fromFilename = session.cwd + '/' + fromFilename;
+    }
+  }
+
+  // Does the file exist?
+  fs.exists(fromFilename, function (exists) {
+    if (exists) {
+      session.rnfr = fromFilename;
+      output.write(350, 'File or directory exists, ready for destination name');
+    } else {
+      session.rnfr = false;
+      output.write(550, '%s: No such file or directory', fromFilename);
+    }
+  });
+}
---
+++ commands/rnto.js
@@ -0,0 +1,72 @@
+var command = require('../lib/command');
+var fs      = require('fs');
+
+command.add('RNTO', rnto, {maxArguments: 1, minArguments: 1});
+
+/**
+ * Used when renaming a file. After sending an RNFR command to specify the file
+ * to rename, send this command to specify the new name for the file.
+ * @param {!string} toFilename
+ * @param {!object} output
+ * @param {!object} session
+ */
+function rnto(toFilename, output, session) {
+  // If to name is a relative path, prepend the CWD to it to get an absolute
+  // path
+  if (toFilename[0] !== '/') {
+    if (session.cwd === '/') {
+      toFilename = session.cwd + toFilename;
+    } else {
+      toFilename = session.cwd + '/' + toFilename;
+    }
+  }
+
+  // Did the user issue an RNFR first?
+  if (!session.rnfr) {
+    output.write(503, 'Bad eqeuence of commands');
+    return;
+  }
+
+  // Move the file
+  fs.rename(session.rnfr, toFilename, function (err) {
+    if (err) {
+      // See `unlink(2)` for a complete list of possible errors and their
+      // meanings. We don't want to give too much information away, so
+      // certain rare errors that have nothing to do with user input just
+      // display a generic error (Like EIO, EFAULT, etc)
+      switch (err.code) {
+        case 'EACCES':
+          output.write(550, '%s: Permission denied', toFilename);
+          break;
+        case 'EBUSY':
+          output.write(550, '%s: In use by the system or another process', toFilename);
+          break;
+        case 'EISDIR':
+          output.write(550, '%s: Is a directory', toFilename);
+          break;
+        case 'ELOOP':
+          output.write(550, '%s: Too many symbolic links', toFilename);
+          break;
+        case 'ENAMETOOLONG':
+          output.write(550, '%s: Too long', toFilename);
+          break;
+        case 'ENOENT':
+          output.write(550, '%s: No such file or directory', toFilename);
+          break;
+        case 'ENOTDIR':
+          output.write(550, '%s: Directory is not a directory', toFilename);
+          break;
+        case 'EPERM':
+          output.write(550, '%s: Permission denied', toFilename);
+          break;
+        case 'EROFS':
+          output.write(550, '%s: Read-only file system, cannot delete', toFilename);
+          break;
+        default:
+          output.write(550, '%s: An error occured', toFilename);
+      }
+    } else {
+      output.write(250, 'Rename successful');
+    }
+  });
+}
