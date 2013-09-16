bb614d724603865f6ac0e136dd432a1a96e7d218
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-16 11:26:21 -0300

    Updating the FEAT command

--- commands/feat.js
+++ commands/feat.js
@@ -2,12 +2,10 @@ var command = require('../lib/command');
 
 command.add('FEAT', 'FEAT (returns feature list)', function (parameters, output, session) {
   output.write(211, '-Features');
-  output.write('SIZE');
   output.write('MDTM');
-  output.write('LANG EN*');
+  output.write('MLST modify*;perm*;size*;type*;unique*;UNIX.group*;UNIX.mode*;UNIX.owner*;');;
   output.write('REST STREAM');
-  output.write('TVFS');
   output.write('PASV');
-  output.write('UTF8');
+  output.write('SIZE');
   output.write(211, 'end');
 });
