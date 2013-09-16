bbae20d6d18df486ed55c2645f5f371c344ad219
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-16 08:19:49 -0300

    Get the user's groups in the PAM module

--- auth/pam.js
+++ auth/pam.js
@@ -2,7 +2,43 @@
 var auth   = require('../lib/auth');
 var config = require('../lib/config');
 var fs     = require('../lib/fs');
+var rfs    = require('fs');
+var _      = require('lodash');
 var pam    = require('authenticate-pam');
+
+// Variables
+var users = {};
+var groups = {};
+
+// Parse and cache /etc/passwd
+rfs.readFile('/etc/passwd', { encoding: 'UTF-8' }, function (err, data) {
+  if (!err) {
+    var _users = data.split('\n');
+
+    _.map(_users, function (userRow) {
+      var user = userRow.split(':');
+
+      if (user[0] !== '') {
+        users[user[0]] = user;
+      }
+    });
+  }
+});
+
+// Parse and cache /etc/group
+rfs.readFile('/etc/group', { encoding: 'UTF-8' }, function (err, data) {
+  if (!err) {
+    var _groups = data.split('\n');
+
+    _.map(_groups, function (groupRow) {
+      var group = groupRow.split(':');
+
+      if (group[0] !== '') {
+        groups[group[0]] = group;
+      }
+    });
+  }
+});
 
 // Register our authentication mechanism
 auth.register('pam', function (username, password, done) {
@@ -10,18 +46,27 @@ auth.register('pam', function (username, password, done) {
     if (err) {
       done(err, null);
     } else {
-      fs.readFile('/etc/passwd', { encoding: 'UTF-8' }, function (err, data) {
-        var userDetails = data.match(new RegExp('^(' + username + '.*)', 'im'))[1].split(':');
+      // Get all of the groups the user is part of
+      var userGroups = [users[username][3]];
 
-        var user = {
-          username: username,
-          password: password,
-          chroot: config.get('auth.chroot') === '~' ? userDetails[5] : config.get('auth.chroot'),
-          home: userDetails[5]
-        };
+      _.map(groups, function (group) {
+        var usersInGroup = group[3].split(',');
 
-        done(err, user);
+        if (usersInGroup.indexOf(username) !== -1) {
+          userGroups.push(group[2]);
+        }
       });
+
+      // Format and return the user data
+      var user = {
+        username: username,
+        password: password,
+        chroot: config.get('auth.chroot') === '~' ? users[username][5] : config.get('auth.chroot'),
+        home: users[username][5],
+        groups: userGroups
+      };
+
+      done(err, user);
     }
   });
 });
