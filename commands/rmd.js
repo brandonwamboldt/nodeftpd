e56d24bb4d57cfc595a655085df280db4c5998aa
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-17 06:35:57 -0300

    Adding better error messages for RMD

--- commands/rmd.js
+++ commands/rmd.js
@@ -22,10 +22,14 @@ command.add('RMD', 'RMD <sp> pathname', function (rmdir, output, session) {
     fs.rmdir(rmdir, function (err) {
       if (err) {
         if (err.code === 'ENOENT') {
-          output.write(550, rmdir + ': File or directory does not exist');
+          output.write(550, '%s: File or directory does not exist', rmdir);
+        } else if (err.code === 'ENOTEMPTY') {
+          output.write(550, '%s: Directory not empty, cannot remove', rmdir);
+        } else if (err.code === 'EACCES') {
+          output.write(550, '%s: Permission denied', rmdir);
         } else {
           console.log(err);
-          output.write(550, rmdir + ': Unknown error');
+          output.write(550, '%s: Unknown error', rmdir);
         }
       } else {
         output.write(250, 'RMD command successful');
