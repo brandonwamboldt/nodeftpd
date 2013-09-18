f67ae9f81248050f456b635d9f63e2ad920265d8
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-18 12:30:52 -0300

    Disable MLST/MLSD until they are done

--- commands/feat.js
+++ commands/feat.js
@@ -3,7 +3,7 @@ var command = require('../lib/command');
 command.add('FEAT', 'FEAT (returns feature list)', function (parameters, output, session) {
   output.write(211, '-Features');
   output.write('MDTM');
-  output.write('MLST modify*;perm*;size*;type*;unique*;UNIX.group*;UNIX.mode*;UNIX.owner*;');;
+  //output.write('MLST modify*;perm*;size*;type*;unique*;UNIX.group*;UNIX.mode*;UNIX.owner*;');;
   output.write('REST STREAM');
   output.write('PASV');
   output.write('SIZE');
