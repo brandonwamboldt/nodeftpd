7f4b6c651c77fcf734db857ba3b414d909f9312a
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-19 06:15:19 -0300

    Show errors for STOR before the upload completes

--- commands/stor.js
+++ commands/stor.js
@@ -3,34 +3,41 @@ var channel = require('../lib/datachannel');
 var fs      = require('../lib/fs');
 
 command.add('STOR', 'STOR <sp> pathname', function (path, output, session) {
-  var success = channel.create(session, function (socket, done) {
-    // @todo Write to file as it is received to avoid memory issues
-    var buffers = [];
-    var bytes   = 0;
-    var offset  = 0;
+  var absolutePath = fs.toAbsolute(path, session.cwd);
 
-    socket.on('data', function (buffer) {
-      buffers.push(buffer);
-      bytes += buffer.length;
-    });
+  fs.open(absolutePath, 'w', function (err, fd) {
+    if (err) {
+      err = fs.errorMessage(err, path);
+      output.write(err.status, err.msg);
+    } else {
+      var success = channel.create(session, function (socket, done) {
+        // @todo Write to file as it is received to avoid memory issues
+        var buffers = [];
+        var bytes   = 0;
+        var offset  = 0;
 
-    socket.on('end', function (data) {
-      fs.open(session.cwd + '/' + path, 'w', function (err, fd) {
-        for (var i in buffers) {
-          fs.writeSync(fd, buffers[i], 0, buffers[i].length, offset);
-          offset += buffers[i].length;
-        }
+        socket.on('data', function (buffer) {
+          buffers.push(buffer);
+          bytes += buffer.length;
+        });
 
-        done();
+        socket.on('end', function (data) {
+          for (var i in buffers) {
+            fs.writeSync(fd, buffers[i], 0, buffers[i].length, offset);
+            offset += buffers[i].length;
+          }
 
-        output.write(226, 'Transfer Complete');
+          done();
+
+          output.write(226, 'Transfer Complete');
+        });
       });
-    });
-  });
 
-  if (!success) {
-    output.write(425, 'Unable to build data connection: Invalid argument');
-  } else {
-    output.write(150, 'Opening ' + session.transferType.toUpperCase() + ' mode data connection for ' + path);
-  }
+      if (!success) {
+        output.write(425, 'Unable to build data connection: Invalid argument');
+      } else {
+        output.write(150, 'Opening ' + session.transferType.toUpperCase() + ' mode data connection for ' + path);
+      }
+    }
+  });
 });
