b14d83097103ca17143cf84bd8e023cb169d032f
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-22 23:11:20 -0300

    Comments + cleanup of QUIT

--- commands/quit.js
+++ commands/quit.js
@@ -1,7 +1,22 @@
+// Local dependencies
 var command = require('../lib/command');
-var os      = require('os');
 
-command.add('QUIT', 'QUIT (close control connection)', function (p, output) {
-  output.write(221, 'Goodbye');
-  output.close();
+/**
+ * A QUIT request asks the server to close the connection:
+ *
+ *     220 Features: a .
+ *     QUIT
+ *     221 Bye.
+ *
+ * QUIT parameters are prohibited. Some servers will reject any QUIT request
+ * with a nonempty parameter.
+ *
+ * If the server accepts the QUIT request (required code 221), it then closes
+ * the connection without reading further requests. The server also stops
+ * listening for data connections and drops any data connections already
+ * accepted.
+ */
+command.add('QUIT', 'QUIT (close control connection)', function (nil, commandChannel) {
+  commandChannel.write(221, 'Goodbye');
+  commandChannel.close();
 });
