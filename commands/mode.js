2374028ddf88a857033bd0bd9ad5ae59fbf7b3fe
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-22 21:51:08 -0300

    Comments for MODE

--- commands/mode.js
+++ commands/mode.js
@@ -1,13 +1,19 @@
+// Local dependencies
 var command = require('../lib/command');
 
-command.add('MODE', 'Syntax: MODE is not implemented (always S)', { maxArguments: 1, minArguments: 1 }, function (mode, output) {
+/**
+ * MODE is obsolete. The server should accept MODE S (in any combination of
+ * lowercase and uppercase) with code 200, and reject all other MODE attempts
+ * with code 504.
+ */
+command.add('MODE', 'Syntax: MODE is not implemented (always S)', function (mode, commandChannel) {
   mode = mode.toUpperCase();
 
   if (mode === 'S') {
-    output.write(200, 'Mode set to S');
+    commandChannel.write(200, 'Mode set to S');
   } else if (mode === 'A' || mode === 'C') {
-    output.write(504, '\'MODE ' + mode + '\' unsupported transfer mode');
+    commandChannel.write(504, '\'MODE ' + mode + '\' unsupported transfer mode');
   } else {
-    output.write(500, '\'MODE ' + mode + '\' unrecognized transfer mode');
+    commandChannel.write(500, '\'MODE ' + mode + '\' unrecognized transfer mode');
   }
 });
