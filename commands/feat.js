532a3241c0ad4edb50614f596f92d1802b08180b
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-09-27 12:06:48 -0300

    Add TLS related features to FEAT

--- commands/feat.js
+++ commands/feat.js
@@ -10,9 +10,14 @@ var command = require('../lib/command');
 command.add('FEAT', 'FEAT (returns feature list)', function (nil, commandChannel) {
   commandChannel.write(211, '-Features');
   commandChannel.write('MDTM');
+  //commandChannel.write('MFMT');
+  //commandChannel.write('AUTH TLS');
+  //commandChannel.write('MFF modify;UNIX.group;UNIX.mode;');
   commandChannel.write('MLST modify*;perm*;size*;type*;unique*;UNIX.group*;UNIX.mode*;UNIX.owner*;');
-  commandChannel.write('REST STREAM');
   commandChannel.write('PASV');
+  commandChannel.write('PBSZ');
+  commandChannel.write('PROT');
+  commandChannel.write('REST STREAM');
   commandChannel.write('SIZE');
   commandChannel.write(211, 'end');
 });
