ae96c7cf13d05b827c6820cf0ae80fce4d2dc558
Brian M. Jemilo II <jemiloii@jemiloii.com>
2015-12-08 09:59:39 -0600

    Add Redhat package name
    For Fedora/Redhat/Centos yum installs. http://stackoverflow.com/questions/18906019/check-if-libpam-headers-are-available

--- bin/install_check.sh
+++ bin/install_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ `echo '#include <security/pam_appl.h>' | cpp -H -o /dev/null 2>&1 | head -n1 | grep 'fatal error' | wc -l` == "1" ]; then
-  echo "nodeftpd: Please install the libpam-dev package";
+  echo "nodeftpd: Please install the libpam-dev (or pam-devel) package";
   exit 1;
 fi
